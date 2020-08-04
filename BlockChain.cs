d57c702c771a4ce857b8b9dc910192969a7b9c89
Joseph Niccoli <30275962+JosephNiccoli@users.noreply.github.com>
2020-08-04 16:58:03 -0700

    Add project files.

---
+++ BlockChain.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstBlockChain
+{
+    public class BlockChain
+    {
+        public IList<Block> Chain { set; get; }
+
+        public BlockChain()
+        {
+            InitializeChain();
+            AddGenesisBlock(); 
+        }
+
+        public void InitializeChain()
+        {
+            Chain = new List<Block>();
+        }
+
+        public Block CreateGenesisBlock()
+        {
+            return new Block(DateTime.Now, null, "{}");
+        }
+        
+        public void AddGenesisBlock()
+        {
+            Chain.Add(CreateGenesisBlock());
+        }
+
+        public Block GetLatestBlock()
+        { 
+            return Chain[Chain.Count - 1];
+        }
+    }
+}
