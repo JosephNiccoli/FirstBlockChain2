4f777fc6c97ef15712e1a9cd45a5b06e9faad2e0
Joseph Niccoli <30275962+JosephNiccoli@users.noreply.github.com>
2020-08-22 09:49:51 -0700

    p2pClient Added
    https://www.c-sharpcorner.com/article/building-a-blockchain-in-net-core-p2p-network/

---
+++ P2PServer.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Server.IIS.Core;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace FirstBlockChain.P2P
+{
+    public class P2PServer : WebSocketBehavior
+    {
+        bool chainSynched = false;
+        WebSocketServer wss = null;
+
+        public void Start()
+        {
+            wss = new WebSocketServer($"ws://127.0.0.1:{Program.Port}");
+            wss.AddWebSocketService<P2PServer>("/BlockChain");
+            wss.Start();
+            Console.WriteLine($"Started server at ws://127.0.0.1:{Program.Port}");
+        }
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            if (e.Data == "Hi Server")
+            {
+                Console.WriteLine(e.Data);
+                Send("hi Client");
+            }
+            else
+            {
+                BlockChain newChain = JsonConvert.DeserializeObject<BlockChain>(e.Data);
+                if (newChain.IsValid() && newChain.Chain.Count > Program.GhostCoin.Chain.Count)
+                {
+                    List<Transaction> newTransactions = new List<Transaction>();
+                    newTransactions.AddRange(newChain.PendingTransactions);
+                    newTransactions.AddRange(Program.GhostCoin.PendingTransactions);
+
+                    newChain.pendingTransactions = newTransactions;
+                    InvalidProgramException.GhostCoin = newChain;
+                }
+
+                if (!chainSynched)
+                {
+                    SendCompletedEventHandler(JsonConvert.SeralizedObject(InvalidProgramException.GhostCoin));
+                    chainSynched = true;
+                }
+            }
+        }
+    }
+}
